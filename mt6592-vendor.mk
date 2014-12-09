90f5a6f2e283fe9dde9d070a2f55cbfcafd48680
Alexey Kuznetsov <axet@me.com>
2014-12-09 18:27:59 +0300

    fix paths

--- mt6592-vendor.mk
+++ mt6592-vendor.mk
@@ -4,9 +4,10 @@ PRODUCT_COPY_FILES += \
     vendor/mediatek/mt6592/lib/egl/libEGL_mali.so:system/lib/egl/libEGL_mali.so \
     vendor/mediatek/mt6592/lib/hw/libaudio.usb.default.so:system/hw/libaudio.usb.default.so \
     vendor/mediatek/mt6592/lib/hw/hwcomposer.mt6592.so:system/lib/hw/hwcomposer.mt6592.so \
-    vendor/mediatek/mt6592/lib/hw/hwcomposer.mt6592.so:system/lib/hw/hwcomposer.mt6592.so \
+    vendor/mediatek/mt6592/lib/hw/gralloc.mt6592.so:system/lib/hw/gralloc.mt6592.so \
     vendor/mediatek/mt6592/lib/hw/libaudio.r_submix.default.so:system/lib/hw/libaudio.r_submix.default.so \
     vendor/mediatek/mt6592/lib/libdpframework.so:system/lib/libdpframework.so \
     vendor/mediatek/mt6592/lib/libion.so:system/lib/libion.so \
     vendor/mediatek/mt6592/lib/libMali.so:system/lib/libMali.so \
+    vendor/mediatek/mt6592/lib/libm4u.so:system/lib/libm4u.so
 
