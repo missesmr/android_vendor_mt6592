cbff9028cb8b20868717cb8a661f432f79e824d8
Alexey Kuznetsov <axet@me.com>
2014-12-09 17:28:24 +0300

    add initial vendor stuff

---
+++ mt6592-vendor.mk
@@ -0,0 +1,12 @@
+PRODUCT_COPY_FILES += \
+    vendor/mediatek/mt6592/lib/egl/libGLESv1_CM_mali.so:system/lib/egl/libGLESv1_CM_mali.so \
+    vendor/mediatek/mt6592/lib/egl/libGLESv2_mali.so:system/lib/egl/libGLESv2_mali.so \
+    vendor/mediatek/mt6592/lib/egl/libEGL_mali.so:system/lib/egl/libEGL_mali.so \
+    vendor/mediatek/mt6592/lib/hw/libaudio.usb.default.so:system/hw/libaudio.usb.default.so \
+    vendor/mediatek/mt6592/lib/hw/hwcomposer.mt6592.so:system/lib/hw/hwcomposer.mt6592.so \
+    vendor/mediatek/mt6592/lib/hw/hwcomposer.mt6592.so:system/lib/hw/hwcomposer.mt6592.so \
+    vendor/mediatek/mt6592/lib/hw/libaudio.r_submix.default.so:system/lib/hw/libaudio.r_submix.default.so \
+    vendor/mediatek/mt6592/lib/libdpframework.so:system/lib/libdpframework.so \
+    vendor/mediatek/mt6592/lib/libion.so:system/lib/libion.so \
+    vendor/mediatek/mt6592/lib/libMali.so:system/lib/libMali.so \
+
